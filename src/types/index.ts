33724258681b576ea776a7d303caa12790129e9a
abouhid2 <alexandre.bouhid@umode.com.br>
2025-05-18 14:20:39 -0300

    first commit

---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export interface Event { id: number; title: string; description: string; date: string; location: string; } export interface Participation { id: number; eventId: number; userId: number; status: "going" | "not_going" | "maybe"; }
